e36197369867e31c95d7cf0be4184be1946facbc
maina68 <mwangisgeorge@gmail.com>
2022-12-08 17:03:41 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return:always 0
+ */
+int main(void)
+{
+	printf("Size of char:%i byte(s)\n", sizeof(char));
+	printf("Size of char:%i byte(s)\n", sizeof(char));
+	printf("Size of char:%i byte(s)\n", sizeof(char));
+	printf("Size of char:%i byte(s)\n", sizeof(char));
+	printf("Size of char:%i byte(s)\n", sizeof(char));
+	return (0);
+}
