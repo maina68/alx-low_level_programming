cc8a63db2fa541baf8b5f1e96d712d0c602fbea6
maina68 <mwangisgeorge@gmail.com>
2022-12-09 17:53:22 -0800

    tebahpla

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 /**
  *  main - Entry 
- *  Return: Always (0) (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
 int num = '0';
+
 while (num <= '9')
 {
 putchar(num);
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  main - Entry
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
