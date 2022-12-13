b9f0358b029f7cfa2c851fe4b1756dae0cf400d3
maina68 <mwangisgeorge@gmail.com>
2022-12-13 14:53:18 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	char ch = "maina";
+
+	while (ch)
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
