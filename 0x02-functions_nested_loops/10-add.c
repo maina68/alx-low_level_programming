37118e13d245bd7c7972bd15c3af1303381b0a3e
maina68 <mwangisgeorge@gmail.com>
2022-12-15 15:39:55 -0800

    adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers
+ *
+ * @num1: first integer
+ * @num2: second interger
+ *
+ * Return: sum of @num1 and @num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
