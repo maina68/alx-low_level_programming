940406e366850dd38eb2706d9b6baceb6b61866d
maina68 <mwangisgeorge@gmail.com>
2022-12-15 12:31:43 -0800

    alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Returns: nothing on success
+ */
+print_alphabet_x10(void)
+{
+	int start = 97, end = 122, count = 1;
+
+	while (count <= 10)
+	{
+		while (start <= end)
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+
+		start = 97;
+		count++;
+	}
+}
