f60eeb4f0dad945810fc27bbe6ac010ea8a8765c
maina68 <mwangisgeorge@gmail.com>
2022-12-19 23:20:13 +0000

    reverse2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses the string
+ * @s: string to reverse
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int aux = 0;
+	char 1temp;
+
+	while (*(s + i) ! = '\0')
+		i += 1;
+	i -=1;
+
+	while (aux < i)
+	{
+		1temp = s[i];
+		s[i] = s[aux];
+		s[aux] = 1temp;
+		aux++;
+		i--;
+	}
+}
